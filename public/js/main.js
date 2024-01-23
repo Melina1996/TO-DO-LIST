8e763a742974cff014d1261f949fe27f2a8977d8
Melina1996 <melina.schlachter@gmx.de>
2024-01-23 11:49:21 +0100

    added filter function JS

--- public/js/main.js
+++ public/js/main.js
@@ -13,6 +13,8 @@ function addTask(){
   let newDiv = document.createElement("div")
 
   newDiv.setAttribute("class","each-task")
+
+  newDiv.setAttribute("status","in-process")
   
   newDiv.innerHTML = myModelDiv.innerHTML
   
@@ -33,10 +35,8 @@ btnAdd.addEventListener("click",addTask)
 
 //event listener for whole section
 
-
-
 eachTask.addEventListener("click",(e)=>{
-  console.log(e.target)
+  
   if (e.target.id == "validate") {
     console.log(e.target.id)
     let myInput = e.target.parentElement.parentElement.querySelector(".modify-task-input")
@@ -44,11 +44,11 @@ eachTask.addEventListener("click",(e)=>{
     e.target.parentElement.parentElement.querySelector(".my-text").innerText = myInput.value
     myInput.classList.add("hide")
 
-    e.target.parentElement.parentElement.setAttribute("status","validated")
+    e.target.parentElement.parentElement.setAttribute("status","done")
     e.target.parentElement.parentElement.style.background = "blue"
 
   } else if(e.target.id == "modify") {
-    e.target.parentElement.parentElement.setAttribute("status","in process")
+    e.target.parentElement.parentElement.setAttribute("status","in-process")
     let myInput = e.target.parentElement.parentElement.querySelector(".modify-task-input")
     myInput.setAttribute("class","modify-task-input")
     myInput.value = e.target.parentElement.parentElement.querySelector(".my-text").innerText
@@ -58,5 +58,56 @@ eachTask.addEventListener("click",(e)=>{
   } 
 })
 
+//event listener for filtering
+
+let btnAll = document.querySelector("#all")
+
+
+let btnProcess = document.querySelector("#in-process")
+
+let btnDone = document.querySelector("#done")
+
+//all my tasks in process
+let elementsProcess = document.querySelectorAll("[status=in-process]")
+
+//all DONE elements
+let elementsDone = document.querySelectorAll("[status=done]")
+
+
+btnProcess.addEventListener("click",()=>{
+  //status of all my tasks 
+  elementsDone = document.querySelectorAll("[status=done]")
+  elementsProcess = document.querySelectorAll("[status=in-process]")
+  elementsDone.forEach(element => {
+    element.classList.add("hide")
+  });
+  elementsProcess.forEach(element => {
+    element.classList.remove("hide")
+  });
+})
+
+btnDone.addEventListener("click",()=>{
+  //status of all my tasks
+elementsDone = document.querySelectorAll("[status=done]")
+elementsProcess = document.querySelectorAll("[status=in-process]")
+  elementsProcess.forEach(element => {
+    element.classList.add("hide")
+  });
+  elementsDone.forEach(element => {
+    element.classList.remove("hide")
+  });
+})
+
+btnAll.addEventListener("click",()=>{
+  //status of all my tasks 
+  elementsDone = document.querySelectorAll("[status=done]")
+  elementsProcess = document.querySelectorAll("[status=in-process]")
+  elementsDone.forEach(element => {
+    element.classList.remove("hide")
+  });
+  elementsProcess.forEach(element => {
+    element.classList.remove("hide")
+  });
+})
 
 
