cae22a9cef7542e10cddc7876f62124f5d54ffa4
Melina1996 <melina.schlachter@gmx.de>
2024-01-23 11:59:34 +0100

    adjusted JS

--- public/js/main.js
+++ public/js/main.js
@@ -38,20 +38,29 @@ btnAdd.addEventListener("click",addTask)
 eachTask.addEventListener("click",(e)=>{
   
   if (e.target.id == "validate") {
-    console.log(e.target.id)
+
     let myInput = e.target.parentElement.parentElement.querySelector(".modify-task-input")
-    console.log(myInput.value)
-    e.target.parentElement.parentElement.querySelector(".my-text").innerText = myInput.value
-    myInput.classList.add("hide")
+
+    if(myInput.value.length != 0){
+      e.target.parentElement.parentElement.querySelector(".my-text").innerText = myInput.value
+      myInput.classList.add("hide")
+    } 
 
     e.target.parentElement.parentElement.setAttribute("status","done")
-    e.target.parentElement.parentElement.style.background = "blue"
 
+    e.target.parentElement.parentElement.style.background = "blue"
+    
   } else if(e.target.id == "modify") {
+    e.target.parentElement.parentElement.style.background = "white"
+
     e.target.parentElement.parentElement.setAttribute("status","in-process")
     let myInput = e.target.parentElement.parentElement.querySelector(".modify-task-input")
+    //take "hide"-class away
     myInput.setAttribute("class","modify-task-input")
+    //want to retake the before entered text as value to be able to mmodify it in the field
     myInput.value = e.target.parentElement.parentElement.querySelector(".my-text").innerText
+    console.log(myInput)
+    console.log(myInput.value)
   } else if(e.target.id == "delete"){
     console.log(e.target.id)
     e.target.parentElement.parentElement.remove()
